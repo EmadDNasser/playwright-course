7d6f0cfe354c4fa7a0c0486278a1cf6773d229bd
EmadDNasser <emad-nsr@hotmail.com>
2026-01-04 23:04:45 +0400

    Create the playwright

---
+++ tests/basics.spec.ts
@@ -0,0 +1,6 @@
+import {expect, test} from "@playwright/test";
+
+test("check the title", async({page}) => {
+await page.goto("https://playwright.dev/");
+await expect(page).toHaveTitle("Fast and reliable end-to-end testing for modern web apps | Playwright");
+});
